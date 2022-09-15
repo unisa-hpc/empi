b92774b3408c0103f159453b684d2d1d8098d73f
Biagio Cosenza <bcosenza@unisa.it>
2022-09-15 16:43:51 +0200

    Create empi_bidir_ring.c

---
+++ examples/empi_bidir_ring.c
@@ -0,0 +1,23 @@
+#include "empi.h"
+#include <iostream>
+
+int main(argc,argv) {  
+  empi::Context ctx(&argc, &argv);
+  
+  int prev = ctx.rank()-1;
+  int next = ctx.rank()+1;
+  if (ctx.rank() == 0) prev = ctx.size() - 1;
+  if (ctx.rank() == (ctx.size() - 1)) next = 0;
+
+  const tag1 = 1;
+  const tag2 = 2;
+  
+   // with fixed-tag and type message context
+  ctx.runAndWait<type>([](empi::TypeWaitContext &ttw_ctx){  
+      ttw_ctx.irecv<tag1>(&buf[0], 1, prev);
+      ttw_ctx.irecv<tag2>(&buf[1], 1, next);
+      ttw_ctx.isend<tag1>(&buf[1], 1, prev);
+      ttw_ctx.isend<tag2>(&buf[0], 1, next);
+  }); // Waitall is implicit here
+  return 0; // Finalize is implicti here
+}
