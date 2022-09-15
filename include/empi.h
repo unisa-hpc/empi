afd29e2cca58461bf655fa2675279a66da7f3c21
Biagio Cosenza <bcosenza@unisa.it>
2022-09-15 16:53:24 +0200

    Update empi.h

--- include/empi.h
+++ include/empi.h
@@ -1,1 +1,54 @@
 // first draft
+
+namespace empi {
+
+ 
+ public class Context {
+  private: 
+    int rank;
+    int size;
+    int comm;
+ 
+   
+   send()
+      
+    recv()
+      
+    isend()     
+      
+    irecv()
+   
+   
+ };
+  
+  
+  template <Tag, Type>
+  public class TagTypeContext {
+  
+    send()
+      
+    recv()
+      
+    isend()     
+      
+    irecv()
+    
+  };
+  
+  
+  
+  template <Tag, Type>
+  public class WaitContext {
+  
+    send()
+      
+    recv()
+      
+    isend()     
+      
+    irecv()
+    
+  };
+  
+
+};
