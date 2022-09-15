57b38837fe27cab1488b0d77d0ddda29a943aeaa
Biagio Cosenza <bcosenza@unisa.it>
2022-09-15 15:49:51 +0200

    Create mpi_helloworld.c
    first mpi hello world

---
+++ examples/mpi_helloworld.c
@@ -0,0 +1,24 @@
+#include <mpi.h>  
+#include <stdio.h>
+
+int main(int argc, char **argv){
+  int myid, procs, err;
+
+	// initialize MPI_Init
+	err = MPI_Init(&argc, &argv);
+	if (err != MPI_SUCCESS){
+    printf("\nError initializing MPI.\n");
+	  MPI_Abort(MPI_COMM_WORLD, err);
+  }
+
+	// Get No. of processors
+	MPI_Comm_size(MPI_COMM_WORLD,&procs);
+		
+	// Get processor id		
+	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+
+	printf("\tI am processor %d: Total No. of processors =  %d.\n", myid, procs);
+	printf("\tHello from processor %d.\n", myid);
+	MPI_Finalize();
+	return 0;  
+}
