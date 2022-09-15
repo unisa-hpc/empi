b20588d8af7853b98caeda6dc473f8eac77cedb4
Biagio Cosenza <bcosenza@unisa.it>
2022-09-15 15:56:19 +0200

    Create empi_helloworld.cpp

---
+++ examples/empi_helloworld.cpp
@@ -0,0 +1,14 @@
+// TODO: error handling
+
+#include <empi.h>  
+#include <iostream>
+
+int main(int argc, char **argv){
+  
+  EMPI_Context ctx(&argc, &argv); 
+  printf("\tI am processor %d: Total No. of processors =  %d.\n", ctx.rank(), ctx.size());
+	printf("\tHello from processor %d.\n", ctx.rank());
+
+	return 0;  
+}
+
