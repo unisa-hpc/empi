01be0fdbdfac08eaced1e3c32fa09e287ff2f7e0
Biagio Cosenza <bcosenza@unisa.it>
2022-09-20 16:13:22 +0200

    Update empi_bidir_ring.cpp

--- examples/empi_bidir_ring.cpp
+++ examples/empi_bidir_ring.cpp
@@ -11,13 +11,14 @@ int main(argc,argv) {
 
   const tag1 = 1;
   const tag2 = 2;
+  const int size = 1
   
    // with fixed-tag and type message context
-  ctx.run_and_wait<type>([](empi::MessageGroupHandler &mgh){  
-      mgh.irecv<tag1>(&buf[0], 1, prev);
-      mgh.irecv<tag2>(&buf[1], 1, next);
-      mgh.isend<tag1>(&buf[1], 1, prev);
-      mgh.isend<tag2>(&buf[0], 1, next);
+  ctx.run_and_wait<type,size>([](empi::MessageGroupHandler &mgh){  
+      mgh.irecv<tag1>(&buf[0], prev);
+      mgh.irecv<tag2>(&buf[1], next);
+      mgh.isend<tag1>(&buf[1], prev);
+      mgh.isend<tag2>(&buf[0], next);
   }); // Waitall is implicit here
   
   return 0; // Finalize is implicit here
