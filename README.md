b8174d5b8ee51c872bbd4a4462e9bbde0328d998
Luigi Crisci <lcrisci5@studenti.unisa.it>
2023-02-02 18:36:30 +0000

    - Python script first daft for experiment's launch - Readme init

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # EMPI: Enhanced Message Passing Interface in Modern C++
-Enhanced Message Passing Interface in Modern C++
+EMPI is a C++ library that aims to enhance the MPI programming model's interface while providing competitive performance. Its objective is to provide a more modern and straightforward interface to MPI while exploiting C++ language features to reduce programming errors and improve performance. It is developed using C++20, exploiting several modern C++ features. 
 
-## TODO:
-- [ ] Pre-allocated circular queue for request pool to reduce insert overhead
-- [ ] Refactor memory ownership of request_pool
+## Requirements
+EMPI is written in C++20, so a valid C++20 compiler is required (e.g. gcc 12). The building system is Cmake, with version 3.0 as the minimun requirement.
+EMPI is shipped with a customized OpenMPI implementation, which is required to exploit the best performance. However, EMPI works with a vanilla OpenMPI as well in case you don't want to compile OpenMPI on your own. To do that, disable the unchecked function in the CMake configuring phase with the flag `-DENABLE_UNCHECKED_FUNCTION=OFF`.
+
 
 
 ## How to Compile and run:
---
+++ scripts/common.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+Copyright (c) 2023, Luigi Crisci
+"""
+import sys
+import command
+import argparse
+
+def cmdline_args():
+        # Make parser object
+    p = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    
+    p.add_argument("--size",
+                   type=int,
+                   help="The number of bytes for the benchmarks in power-of-two (e.g. 4 -> 2^4 bytes) (default: 2)",
+                   required=True)
+    p.add_argument("--app_iter", 
+                   type=int,
+                   help="Number of times the body of the app is repeated. This increases the number of communication in the app (default: 10)",
+                   default=10)
+    p.add_argument("--app_restart", 
+                   type=int,
+                   help="Number of times the app is executed before collecting the mean execution time (default: 10)",
+                   default=10)
+    p.add_argument("--num_proc",
+                   type=int,
+                   default=2,
+                   help="Number of MPI processors. Note that in some benchmarks (ping_pong) this value is ignored due to the app structure")
+    p.add_argument("--ompi_path",
+                   type=str,
+                   default="/opt/openmpi/",
+                   help="mpirun path (default: /opt/openmpi/)")
+    p.add_argument("--root",
+                   action='store_true',
+                   default=False,
+                   help="enable mpi with root user",
+                   )
+    p.add_argument("--bench_path",
+                 type=str,
+                 default="../build/benchmarks/",
+                 help="Path to the benchmark folder (default: ../build/benchmarks)"
+                 )
+    return(p.parse_args())
+
+
+def run_experiment(args,exp_name):
+    print(f"//==----------{exp_name}----------==//")
+    time_sum = 0.0
+    newline="\n" #f-string limitation bypass
+    for i in range(0,args.app_restart,1):
+        print(f'{i+1}..{newline if i==(args.app_restart-1) else ""}',end='',flush=True)
+        res = command.run([
+            f'{args.ompi_path}bin/mpirun',
+            f'{"--allow-run-as-root" if args.root else ""}',
+            '-n',
+            f'{args.num_proc}',
+            f'{args.bench_path}{exp_name}', #App name
+            f'{args.size}',
+            f'{args.app_iter}'
+        ])
+        # print(res.output)
+        time_sum += float(res.output)
+    print(f"Aggregated time: {time_sum}")
+    print(f"Mean: {time_sum/args.app_restart}")
+    command.run(["sleep","1"]) #Wait
+    print("-----------------------------------------------")
+
+if __name__ == '__main__':
+    args = cmdline_args()
+    print(args)
+    run_experiment(args,"ping_pong/mpi_ping_pong")
+
+    print()
