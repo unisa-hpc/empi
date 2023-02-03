7a608c1f3843b87c6d23121daf6c469a33fee4ef
Luigi-Crisci <luigicrisci1997@gmail.com>
2023-02-03 19:30:08 +0000

    Sources for openmpi install added

--- scripts/build_openmpi.sh
+++ scripts/build_openmpi.sh
@@ -1,13 +1,16 @@
-M4_PATH=??
-AUTOCONF_PATH=??
-AUTOMAKE_PATH=??
-LIBTOOL_PATH=??
+M4_PATH=$PWD/m4
+AUTOCONF_PATH=$PWD/autoconf
+AUTOMAKE_PATH=$PWD/automake
+LIBTOOL_PATH=$PWD/libtool
+FLEX_PATH=$PWD/flex
 PREFIX=$PWD/deps
+SOURCES=$PWD/sources
 
-OMPI_PATH=??
+OMPI_PATH=$PWD/ompi-4.1
 OMPI_PREFIX=$PWD/openmpi
 
 PATH=$PATH:$PWD/$PREFIX/bin #Setting install folder
+echo $PATH
 
 cd $M4_PATH;
 ./configure --prefix=$PREFIX;
@@ -25,6 +28,12 @@ make -j install;
 cd ..
 
 cd $LIBTOOL_PATH
+./configure --prefix=$PREFIX;
+make -j install;
+cd ..
+
+cd $FLEX_PATH
+./autogen.pl
 ./configure --prefix=$PREFIX;
 make -j install;
 cd ..
